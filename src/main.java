af091f53439156d04871b1af2c9464265961d632
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2025-02-18 19:28:38 +0100

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class main {
 	public static String[] hierba = { "Maria Juana", "Hierba de los bosques", "La seta feliz", "El oro verde" };
 	public static String[] origen = { "Mexico", "Andorra", "Marrueco", "Palestina" };
@@ -6,14 +7,52 @@ public class main {
 	// Variables publicas extra que considereis
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
 		String nombre, lugar;
 		double cantidad;
-
+		boolean seleccion = true;
+		
+		
 		System.out.println("Buenos dias! Que quieres hacer hoy?");
 
+		while (seleccion = true) {
+			
+		System.out.println("Que quieres hacer?: ");
+		System.out.println("a. Buscar producto por nombre. ");
+		System.out.println("b. Buscar producto por origen. ");
+		System.out.println("c. Ordenar productos por precio de mayor a menor. ");
+		System.out.println("d. Ordenar productos por precio de menor a mayor. ");
+		System.out.println("e. Seleccionar varios productos. ");
+		System.out.println("f. Eliminar el segundo producto de su carrito. ");
+		System.out.println("g. Finalizar compra. ");
+		System.out.println("h. Salir. ");
+
+
+		
 		
 		System.out.println("Elige en nuestro menú una opción del 1 al 8: ");
+		String opcion = scan.nextLine();
 		
+			while(!opcion.equals("a") || !opcion.equals("b") || !opcion.equals("c") || !opcion.equals("d") || !opcion.equals("e") || !opcion.equals("f") || 
+					!opcion.equals("g") || !opcion.equals("h")) {
+				
+				System.out.println("Vuelve a repetir tu eleccion: ");
+				System.out.println("a. Buscar producto por nombre. ");
+				System.out.println("b. Buscar producto por origen. ");
+				System.out.println("c. Ordenar productos por precio de mayor a menor. ");
+				System.out.println("d. Ordenar productos por precio de menor a mayor. ");
+				System.out.println("e. Seleccionar varios productos. ");
+				System.out.println("f. Eliminar el segundo producto de su carrito. ");
+				System.out.println("g. Finalizar compra. ");
+				System.out.println("h. Salir. ");
+
+				System.out.println("Elige otra vez en nuestro menú una opción del 1 al 8: ");
+				opcion = scan.nextLine();
+			}
+		
+		if(opcion.equals("a")) {
+				
+		}
 		
 		System.out.println("Usuario selecciona buscar por nombre");
 		nombre = "Juana";
@@ -47,6 +86,8 @@ public class main {
 
 		System.out.println("Usuario finaliza la compra");
 		// Codigo para finalizar la compra y que salga el total de la compra
+		
+		}
 	}
 	
 	//Funciones extra que considereis necesarias
